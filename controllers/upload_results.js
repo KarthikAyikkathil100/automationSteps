383c924bc7f9e854576fddc58c15dfdc2a598d08
Mind <kartik.ayikkathil@mindinventory.com>
2024-12-02 12:29:59 +0530

    Add fn to process Video Intelligence results

--- controllers/upload_results.js
+++ controllers/upload_results.js
@@ -1,26 +1,31 @@
 const responses = require('@Helpers/responses');
 const { uploadFile } = require('@AwsHelpers/S3/index.js')
-const { getCloudStorageClient, getVideoIntelligenceClient } = require('@Helpers/index.js')
-
+const { dynamoConnection } = require('@AwsHelpers/DynamoDB/index.js')
 
 // TODO: Delte the file from local system
 const handler = async (event, context, callback) => {
     try {
         const reqBody = event;
-        const jobId = reqBody.job_id
         const fileName = reqBody.file_name
-        const status = await checkStatus(jobId)
-        if (status) {
-            await processResults(fileName)
-            let convertDSObj = convertDS(`/tmp/${fileName}`)
-            if (!convertDSObj) convertDSObj = {}
-            // await uploadFile(Buffer.from(JSON.stringify(convertDSObj)), 'media.demo.test', `textBlurFile/${fileName}`, 'application/json')
-            // console.log(convertDSObj)
-        }
-        console.log('Status of Job => ', status)
-        return responses.successResponseData(callback, {
-            status
-        })
+        const routeId = reqBody.route_id
+
+        await processResults(fileName)
+
+        const updatedParams = {
+            TableName: 'dev-Routes',
+            Key: {
+              id: routeId,
+            },
+            UpdateExpression: 'set #textBlurJsonFileNameCol = :val',
+            ExpressionAttributeNames: {
+                '#textBlurJsonFileNam': 'textBlurJsonFileName',
+            },
+            ExpressionAttributeValues: {
+                ':val': `textBlurFile/${fileName}`
+            },
+          };
+          await dynamoConnection.update(updatedParams).promise();
+        return responses.successResponseData(callback, {code: 1})
     } catch (e) { 
         console.log('Error while checking job status')
         console.log(e)
@@ -31,53 +36,23 @@ const handler = async (event, context, callback) => {
     }
 }
 
-
-async function checkStatus(jobId) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const client = await getVideoIntelligenceClient('/automation/google/creds')
-            const [operation] = await client.operationsClient.getOperation({name: jobId})
-            if (operation?.done === true) {
-                // push this to the in-progress arr
-                // inProgressJobData.push(jobId)
-                // waitingJobData = waitingJobData.filter((el) => el.id !== jobId.id)
-                return resolve(true)
-            }
-            return resolve(false)
-        } catch (e) {
-            console.log('error here -- ', e)
-            reject(e)
-        }
-    })
-}
-
-
 async function processResults(fileName) {
     try {
         await downloadFileFromGCS('rtme-videos', fileName, `/tmp/${fileName}`)
         console.log('Downloaded the file to be processed')
+
+        let convertDSObj = convertDS(`/tmp/${fileName}`)
+        if (!convertDSObj) convertDSObj = {}
+        await uploadFile(Buffer.from(JSON.stringify(convertDSObj)), 'media.demo.test', `textBlurFile/${fileName}`, 'application/json')
+
+        // Save the text json file's name in Dynamo DB
         return true
     } catch (e) {
         console.log('Error while processing results')
         console.log(e)
-        return null
-    }
-}
-
-async function downloadFileFromGCS(bucketName, fileName, destFileName) {
-    try {
-        const options = {
-            destination: destFileName,
-        };
-        const storage = await getCloudStorageClient('/automation/google/creds')
-        // Downloads the file and store it locally (configured in 'options')
-        await storage.bucket(bucketName).file(fileName).download(options)
-    } catch (e) {
         throw e;
     }
 }
-
-
 
 function convertDS(filePath) {
     try {
@@ -86,7 +61,6 @@ function convertDS(filePath) {
         if (!rawData || !rawData?.annotation_results || !rawData.annotation_results.length > 0)
             throw 'Error in raw data'
         
-        console.log('correct block')
         const annotation_results = rawData?.annotation_results ?? []
         for (let i = 0; i < annotation_results.length; i++) {
             const annotation_result_instance = annotation_results[i];
@@ -105,7 +79,6 @@ function convertDS(filePath) {
                         if (allTimeRes[time]) {
                             (allTimeRes[time]).push(vertices)
                         } else {
-                            console.log('creating for t => ', time)
                             allTimeRes[time] = [vertices]
                         }
                     }
@@ -114,8 +87,11 @@ function convertDS(filePath) {
         }
         return allTimeRes
     } catch (e) {
-        return null
+        console.log('Error while converting DS')
+        console.log(e)
+        throw e
     }
 }
+
 
 exports.handler = handler;
