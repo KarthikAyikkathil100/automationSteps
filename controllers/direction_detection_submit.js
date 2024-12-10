f3783b50a13a04fffd3d6cacbe9516d59eac2b6c
Mind <kartik.ayikkathil@mindinventory.com>
2024-12-10 11:46:38 +0530

    Add direction detection API

---
+++ controllers/direction_detection_submit.js
@@ -0,0 +1,32 @@
+const responses = require('@Helpers/responses');
+const { axios } = require('@Helpers/index.js')
+const axiosInstance = axios.create({
+    baseURL: `${process.env.baseURL}`,
+    timeout: 5000,
+});
+
+// TODO: Delte the file from local system
+const handler = async (event, context, callback) => {
+    try {
+        const routeId = event.route_id
+        const res = await axiosInstance.post('/direction-detection', {
+            route_id: routeId
+        })
+        if (res.status == 200) {
+            return callback(null, {
+                submitted: true,
+            })
+        } else {
+            return callback(null, {
+                submitted: false,
+                route_id: routeId
+            })   
+        }
+    } catch (e) { 
+        console.log('Error while checking job status')
+        console.log(e)
+        return callback('INternal server error', {})   
+    }
+}
+
+exports.handler = handler;
