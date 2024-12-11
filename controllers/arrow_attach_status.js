f78adf54bbe23d0b711aa20ce6b02e92e82a9635
Mind <kartik.ayikkathil@mindinventory.com>
2024-12-11 16:31:07 +0530

    Add arrow-attchment API to step function

---
+++ controllers/arrow_attach_status.js
@@ -0,0 +1,52 @@
+const { dynamoConnection } = require('@AwsHelpers/DynamoDB/index.js')
+
+
+const handler = async (event, context, callback) => {
+    try {
+        const route_id = event.route_id
+        const status = await checkStatus(route_id)
+        let done = false, error = false;
+        if (status === 'ARROW_ATTACHMENT_SUCCESS') {
+            done = true
+        } else if (status === 'ARROW_ATTACHMENT_ERROR') {
+            error = true
+        }
+        console.log('Status of Job => ', status)
+        return callback(null, {
+            route_id,
+            done,
+            error
+        })
+    } catch (e) { 
+        console.log('Error while checking job status')
+        console.log(e)
+        return callback(e, {
+            route_id,
+            done,
+            error
+        })
+    }
+}
+
+async function checkStatus(routeId) {
+    try {
+        const routeParams = {
+            TableName: 'dev-Routes',
+            Key: {
+              id: routeId,
+            },
+            ProjectionExpression: 'id, processStatus',
+        };
+        const routeData = await dynamoConnection.get(routeParams).promise();
+        if (!routeData?.Item || Object.keys(routeData?.Item).length === 0) {
+            throw new Error('Route not found');
+        }
+        return routeData.Item?.processStatus ?? null
+    } catch (e) {
+        console.log('Error while fetching blur status from DynamoDB')
+        throw e
+    }
+}
+
+
+exports.handler = handler;
